795b112351d45e9b015df0563c428f12589f3fff
chtseng93 <chtseng93@gmail.com>
2024-09-16 19:56:48 +0800

    Test

--- src/main/java/com/example/springboot_kafka_demo/config/KafkaTopicConfig.java
+++ src/main/java/com/example/springboot_kafka_demo/config/KafkaTopicConfig.java
@@ -9,6 +9,7 @@ import org.springframework.kafka.config.TopicBuilder;
 public class KafkaTopicConfig {
 	@Bean
 	public NewTopic javaguidesTopic() {
+		
 		return TopicBuilder.name("javaguides")
 				.build();
 	}
